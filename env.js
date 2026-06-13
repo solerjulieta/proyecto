a1508fbacac8f0791f80a5e124c50ec225d6fea4
Julieta Soler <julieta_soler@hotmail.com>
2026-06-13 20:27:47 -0300

    Se corrige estructura

--- env.js
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv'
-
-config()
-
-export const PORT = process.env.PORT || 8080;
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-export const MONGO_URL = process.env.MONGO_URL || '';
-export const JWT_SECRET = process.env.JWT_SECRET || '';
