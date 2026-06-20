cf52ae601bdcfe3248d9ebea9634a5e31d456c59
Julieta Soler <julieta_soler@hotmail.com>
2026-06-20 18:41:34 -0300

    Se actualiza app.js para usar cookieParser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import healtRouter from './src/routes/health.routes.js'
 import eventsRouter from './src/routes/events.routes.js'
 import sessionsRouter from './src/routes/sessions.routes.js'
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.use('/api/health', healtRouter)
 app.use('/api/events', eventsRouter)
