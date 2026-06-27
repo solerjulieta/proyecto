9714b1d0cc080fa34a69dfc8641527a618ebddfd
Julieta Soler <julieta_soler@hotmail.com>
2026-06-27 12:01:13 -0300

    Se actualiza session.controller con passport

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,36 +1,20 @@
 import { NODE_ENV } from '../config/env.js'
-//import { registerUser } from '../services/user.services.js'
-import SessionService from '../services/session.services.js'
-
-const sessionService = new SessionService()
 
 export const register = async (req, res) => {
-  try {
-    //const newUser = await registerUser(req.body)
-    const newUser = await sessionService.registerUser(req.body)
-    res.status(201).json({ status: 'success', payload: newUser })
-  } catch (error) {
-    const status = error.status || 500
-    res.status(status).json({ status: 'error', message: error.message })
-  }
+  res.status(201).json({ status: 'success', payload: req.user })
 }
 
 export const login = async (req, res) => {
-  try {
-    const token = await sessionService.loginUser(req.body)
+  const token = req.user
 
-    res.cookie('currentUser', token, {
-      httpOnly: true,
-      sameSite: 'lax',
-      maxAge: 3600000,
-      secure: NODE_ENV === 'production'
-    })
+  res.cookie('currentUser', token, {
+    httpOnly: true,
+    sameSite: 'lax',
+    maxAge: 3600000,
+    secure: NODE_ENV === 'production'
+  })
 
-    res.status(200).json({ status: 'success', message: 'Login exitoso.' })
-  } catch (error) {
-    console.error('Error en login:', error.message)
-    res.status(500).json({ status: 'error', message: error.message })
-  }
+  res.status(200).json({ status: 'success', message: 'Login exitoso.' })
 }
 
 export const current = (req, res) => 
