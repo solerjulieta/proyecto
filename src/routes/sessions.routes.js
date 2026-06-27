7676fa9fd3fff7e3f73b4c4288ce2c143a6d9f5b
Julieta Soler <julieta_soler@hotmail.com>
2026-06-27 11:59:10 -0300

    Se actualiza session.routes para utilizar passport

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,12 +1,24 @@
 import { Router } from 'express'
 import { register, login, current, logout } from '../controllers/session.controller.js'
-import { auth } from '../middlewares/auth.middleware.js'
+import passport from '../config/passport.config.js'
 
 const router = Router()
 
-router.post('/register', register)
-router.post('/login', login)
-router.get('/current', auth, current)
+router.post('/register',
+    passport.authenticate('register', { session: false }),
+    register
+)
+
+router.post('/login',
+    passport.authenticate('login',  { session: false }), 
+    login
+)
+
+router.get('/current', 
+    passport.authenticate('current', { session: false }), 
+    current
+)
+
 router.post('/logout', logout)
 
 export default router
