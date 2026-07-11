166174218eebde73e77e379869ed024e5f2d7a3f
Julieta Soler <julieta_soler@hotmail.com>
2026-07-11 18:26:50 -0300

    Se actualiza event.model para que sea para eventos de gym

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -3,14 +3,16 @@ import mongoose from 'mongoose'
 const eventSchema = new mongoose.Schema(
     {
       title:       { type: String, required: true, trim: true },
-      description: { type: String, required: true, trim: true },
-      /*category:    { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },*/
+      /*description: { type: String, required: true, trim: true },*/
+      category:    { type: String, required: true, enum: ['boxeo', 'kickboxing', 'crossfit', 'funcional', 'yoga', 'natación'], trim: true },
       date:        { type: Date, required: true },
+      duration:    { type: Number, required: true, min: 1 },
       location:    { type: String, required: true, trim: true },
       capacity:    { type: Number, required: true, min: 1 },
       /*price:       { type: Number, default: 0, min: 0 },*/
       status:      { type: String, enum: ['draft', 'published', 'cancelled', 'finished'], default: 'published' },
-      organizer:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+      organizer:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+      enrollments: { type: Number, default: 0 }
     },
     { timestamps: true }
 )
