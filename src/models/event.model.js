8c4e975a3e8e327354514c9be3fc7fba212a1ee1
Julieta Soler <julieta_soler@hotmail.com>
2026-07-15 11:57:00 -0300

    Se descomentan opciones en event.model

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose'
 const eventSchema = new mongoose.Schema(
     {
       title:       { type: String, required: true, trim: true },
-      /*description: { type: String, required: true, trim: true },*/
+      description: { type: String, required: true, trim: true },
       category:    { type: String, required: true, enum: ['boxeo', 'kickboxing', 'crossfit', 'funcional', 'yoga', 'natación'], trim: true },
       date:        { type: Date, required: true },
       duration:    { type: Number, required: true, min: 1 },
       location:    { type: String, required: true, trim: true },
       capacity:    { type: Number, required: true, min: 1 },
-      /*price:       { type: Number, default: 0, min: 0 },*/
+      price:       { type: Number, default: 0, min: 0 },
       status:      { type: String, enum: ['draft', 'published', 'cancelled', 'finished'], default: 'published' },
       organizer:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
       enrollments: { type: Number, default: 0 }
